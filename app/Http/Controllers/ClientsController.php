46c6ec4fcc4103ebc940a29abea70551bf023cbf
leonardo <ti.leonardo@restauramundo.com>
2017-07-16 21:47:39 -0300

    Criando busca personalizada

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -24,8 +24,19 @@ class ClientsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $name = $request->get('name');
+        if($name){
+            $this->elasticParams['body'] = [
+                'query' => [
+                    'match' => [
+                        'name' => $name
+                    ]
+                ]
+            ];
+            $this->elasticParams['size'] = 1000;
+        }
         $clients = $this->client->search($this->elasticParams);
         return view('clients.index', compact('clients'));
     }
